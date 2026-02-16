84c2b620c92f6a2861cc9f115ed871ba43f5a9ab
Umair Masood <uwinning50@gmail.com>
2026-02-16 07:38:02 +0500

    added new db config

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined");
+  throw new Error("DATABASE_URL is missing");
 }
+
+console.log("Using DB:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
